a4818fb0b20aa4848720c13f313af420696217ea
iamirsalimi <wonderguy2003@gmail.com>
2022-12-23 02:10:40 +0330

    show pass added

--- js/userPanel.js
+++ js/userPanel.js
@@ -2,9 +2,38 @@ let $ = document
 
 let menuSections = $.querySelectorAll('section')
 let menu = $.querySelectorAll('.userPanel-menu .top-menu li')
+let showPassBtn = $.querySelector('.show-pass') 
+let targetMenu , targetPasswordInput
+
+// onload
+window.addEventListener('DOMContentLoaded',function(){
+    menu.forEach(function(menuElement){
+        menuElement.classList.remove('active')
+    })
+    
+    menuSections.forEach(function(menuSection){
+        menuSection.style.display = 'none'
+    })
+
+    menu[0].className = 'active'
+    menuSections[0].removeAttribute('style')
+})
+
+
+// show pass
+showPassBtn.addEventListener('click',function(event){
+    targetPasswordInput = event.target.parentNode.querySelector('input')        
+    
+    if(targetPasswordInput.type === 'password'){
+        targetPasswordInput.type = 'text'
+        showPassBtn.innerHTML = 'visibility_off'
+    } else {
+        targetPasswordInput.type = 'password'
+        showPassBtn.innerHTML = 'visibility'
+    }
+})
 
 // menu
-
 menu.forEach(function(menuElem){
     menuElem.addEventListener('click',function(event){
         menu.forEach(function(menuElement){
