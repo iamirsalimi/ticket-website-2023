b5250a42afda8995206f25ae475164f0b0471d69
iamirsalimi <wonderguy2003@gmail.com>
2023-01-22 23:15:55 +0330

    select box and cart responsivement get better

--- js/products.js
+++ js/products.js
@@ -1,6 +1,22 @@
 let $ = document
 
 let backBtn = $.querySelector('.back-history')
+const selected = $.querySelector('.selected')
+const optionsContainer = $.querySelector('.options-container')
+const optionsList = $.querySelectorAll('.option')
+
+selected.addEventListener('click',function(){
+    optionsContainer.classList.toggle('active')
+})
+
+optionsList.forEach(function(option){
+    option.addEventListener('click',function(event){
+        selected.querySelector('.selected-title').innerHTML = option.querySelector('label').innerHTML
+        optionsContainer.classList.remove('active')
+    })
+})
+
+
 
 backBtn.addEventListener('click',function(){
     history.back()
